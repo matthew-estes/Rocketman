afda8217e55bf4983bc81e670507523cdfff0ad0
matthew-estes <matthew.estes19@gmail.com>
2024-05-23 09:26:29 -0400

    init

---
+++ js/app.js
@@ -0,0 +1,59 @@
+
+
+
+const key = document.querySelectorAll(".key");
+const mysteryLetters = document.querySelectorAll(".letter");
+const mysteryWord = "SABOTEUR";
+const messageEl = document.querySelector(".message");
+
+
+
+let correctLetters = 0;
+let badAttempts = 0;
+
+
+
+function init() {
+  badAttempts = 0;
+  correctLetters = 0;
+}
+
+function checkForWin() {
+  if (correctLetters == mysteryWord.length) {
+    messageEl.innerText = "Congrats, you won!";
+  } else if (badAttempts == 3) {
+    messageEl.innerText = "Sorry, you lost! The word was" + mysteryWord;
+  }
+}
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+key.forEach((key) => {
+  key.addEventListener("click", (event) => {
+    const clickedKey = event.target.innerText;
+
+    for (let i = 0; i < mysteryWord.length; i++) {
+      if (
+        mysteryWord[i] === clickedKey &&
+        !mysteryLetters[i].classList.contains("guess")
+      ) {
+        mysteryLetters[i].innerText = clickedKey;
+        mysteryLetters[i].classList.add("guess");
+        correctLetters++;
+        console.log("correct attempts:", correctLetters);
+        checkForWin();
+        return;
+      }
+    }
+    if (mysteryWord[i] != clickedKey) {
+      badAttempts++;
+      console.log("bad attempts:", badAttempts);
+      checkForWin();
+      return;
+    }
+  });
+});
+
